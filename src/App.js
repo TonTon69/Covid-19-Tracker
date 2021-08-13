48512b0085e5280f82e189dd791f82063e62192c
TonTon69 <hoanghuynh0609@gmail.com>
2021-08-13 22:44:24 +0700

    covid-19 app tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import CountrySelector from "./components/CountrySelector/index";
+import Highlight from "./components/Highlight/index";
+import Summary from "./components/Summary/index";
+import { useState, useEffect } from "react";
+import { getCountries, getReportByCountry } from "./api/index";
+import { Container, Typography } from "@material-ui/core";
+import { sortBy } from "lodash";
+import moment from "moment";
+import "moment/locale/vi";
+import "@fontsource/roboto";
+
+moment.locale("vi");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+    const [selectedCountryId, setSelectedCountryId] = useState(""); //lưu trữ country hiện tại mà user chọn
+    const [report, setReport] = useState([]);
+
+    useEffect(() => {
+        getCountries().then((res) => {
+            console.log("getCountries", { res });
+            const countries = sortBy(res.data, "Country");
+            setCountries(countries);
+            //load trang thì hiển thị thông tin của VN đầu tiên
+            setSelectedCountryId("vn");
+        });
+    }, []);
+
+    const handleOnChange = (e) => {
+        setSelectedCountryId(e.target.value);
+    };
+
+    useEffect(() => {
+        if (selectedCountryId) {
+            const { Slug } = countries.find(
+                (country) => country.ISO2.toLowerCase() === selectedCountryId
+            );
+            //call api
+            getReportByCountry(Slug).then((res) => {
+                console.log("getReportByCountry", { res });
+                //xóa item cuối cùng
+                res.data.pop();
+                setReport(res.data);
+            });
+        }
+    }, [countries, selectedCountryId]);
+
+    return (
+        <Container style={{ marginTop: 20 }}>
+            <Typography variant="h2" component="h2">
+                SỐ LIỆU COVID-19
+            </Typography>
+            <Typography>{moment().format("LLL")}</Typography>
+            <CountrySelector
+                countries={countries}
+                handleOnChange={handleOnChange}
+                value={selectedCountryId}
+            />
+            <Highlight report={report} />
+            <Summary report={report} />
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/components/CountrySelector/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    FormControl,
+    FormHelperText,
+    InputLabel,
+    makeStyles,
+    NativeSelect,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: `${theme.spacing(3)}px 0`,
+    },
+}));
+
+export default function CountrySelector({ value, handleOnChange, countries }) {
+    return (
+        <FormControl className={useStyles.formControl}>
+            <InputLabel htmlFor="country-selector" shrink>
+                Quốc gia
+            </InputLabel>
+            <NativeSelect
+                value={value}
+                onChange={handleOnChange}
+                inputProps={{
+                    name: "country",
+                    id: "country-selector",
+                }}
+            >
+                {countries.map((country) => {
+                    return (
+                        <option
+                            key={country.ISO2}
+                            value={country.ISO2.toLowerCase()}
+                        >
+                            {country.Country}
+                        </option>
+                    );
+                })}
+            </NativeSelect>
+            <FormHelperText>Lựa chọn quốc gia</FormHelperText>
+        </FormControl>
+    );
+}
